b4b44c7cb25c777eb0de4469847ec6144fdc801f
Khanh261 <giakhanh006@gmail.com>
2024-07-10 16:45:55 +0700

    add layout of scene when first opening

--- app/src/main/java/com/example/travelvalda/MainActivity.java
+++ app/src/main/java/com/example/travelvalda/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.travelvalda;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        loadImage();
+
+    }
+
+    private void loadImage() {
+        ImageView imageView = findViewById(R.id.imageView1);
+        String imageUrl = "https://picsum.photos/200/300";
+        Glide.with(this).load(imageUrl).into(imageView);
     }
 }
